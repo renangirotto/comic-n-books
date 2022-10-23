d884ccc74975013f05c8dca5289d802d48d780a8
Renan Girotto <46816071+renangirotto@users.noreply.github.com>
2022-10-23 01:27:12 -0300

    feat: :construction: refactor layout files and remove unecessary componentes

---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Comic and Books`,
+  description: `Um blog sobre quadrinhos e livros com opniões sem nenhuma relevância.`,
+  url: `#`, // No trailing slash!
+  twitterURL: `https://twitter.com/Celebrimbs`
+}
